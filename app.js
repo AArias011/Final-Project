720206a85348d0b270e79d5a45b9567849f32923
Anthony Arias <aarias011@west-mec.org>
2019-12-11 18:22:26 -0700

    Added all my data into one page:

--- app.js
+++ app.js
@@ -3,38 +3,57 @@ var fetch = require('node-fetch');
 const express = require('express');
 var expbs = require('express-handlebars');
 const app= express();
-var expressLayouts = require('express-ejs-layouts');
+// var expressLayouts = require('express-ejs-layouts');
+// app.use(expressLayouts);
 
-app.use(expressLayouts);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server has started on ${PORT}`))
 
 app.set('view engine', 'ejs');
 
-app.get('/:id' , async(req, res) => {
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/ram', async (req, res) => {
+
     try{
-        ramData = await fetch(`https://rickandmortyapi.com/api/character/${req.params.id}`);
+        ramData = await fetch(`https://rickandmortyapi.com/api/character/`);
 
         const json = await ramData.json();
-
-        // const {...results} = json.results;
-        // console.log('results', results);
-        console.log(json.url);
-        res.render('index', {
-            data: {
-                name: json.name,
-                image: json.image,
-                status: json.status,
-                species: json.species,
-                gender: json.gender
-            }
+        
+        const [...characters] = json.results;
+        
+        // console.log(people);
+        
+        res.render('layout/index', {
+            characters: characters
         })
-
     } catch(error){
         console.log(error);
     }
+
 });
 
-app.get('/about', (req, res) => {
-    res.render('about');
-})
+app.get('/ram/:id' , async(req, res) => {
+    try{
+        charData = await fetch(`https://rickandmortyapi.com/api/character/${req.params.id}`);
+
+        const json = await charData.json();
+        
+        console.log(json);
+        
+        res.render('layout/all', {
+            // characters: json.episode,
+            data: {
+                name: json.name,
+                statues: json.status,
+                species: json.species,
+                gender: json.gender,
+                image: json.image,
+            }
+        })
+    } catch(error){
+        console.log(error);
+    }
+
+});
+
